31e4a359049eb884988493063c19451934f0dc5b
Dominik Maier <domenukk@gmail.com>
2019-08-13 20:35:18 +0200

    Minor fixes

--- startafl.sh
+++ startafl.sh
@@ -2,8 +2,8 @@
 export PATH=$PATH:./AFLplusplus 
 if [ -z "$1" ]; then
 	echo "[*] Unicorefuzz running AFL master node. Supply an id to spawn additional workers."
-	afl-fuzz -U -m none -i afl_inputs -o afl_outputs -t 60000+ -M master -- python harness.py @@ || exit 1
+	afl-fuzz -U -m none -i afl_inputs -o afl_outputs -t 4000+ -M master -- python harness.py @@ || exit 1
 else 
 	echo "[*] Unicorefuzz running AFL worker with id $1"
-	afl-fuzz -U -m none -i afl_inputs -o afl_outputs -t 60000+ -S fuzzer$1 -- python harness.py @@ || exit 1
+	afl-fuzz -U -m none -i afl_inputs -o afl_outputs -t 4000+ -S fuzzer$1 -- python harness.py @@ || exit 1
 fi
--- startvm.sh
+++ startvm.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
-qemu-system-x86_64 -kernel linux/arch/x86_64/boot/vmlinux
+stty intr ^]
+echo "[*] ctrl+c remapped to ctrl+] for host"
 
-
-	echo -drive format=raw,file=./debootstrap/stretch.img \
+qemu-system-x86_64 -drive file=../ARCH.qcow2,if=virtio \
 	-enable-kvm \
 	-net nic -net user,hostfwd=tcp::8022-:22 \
 	-m 4096 \
-	-smp 4 \
+	-smp 1 \
 	-s \
 	-nographic \
-	-kernel linux/arch/x86_64/boot/vmlinux \
+	-kernel linux/arch/x86_64/boot/bzImage \
+	-initrd linux-5.0.13-arch1/arch/x86_64/boot/initramfs.img \
 	-append "root=/dev/vda2 debug console=ttyS0 nokaslr" \
-	-monitor telnet:127.0.0.1:1235,server,nowait \
-	-initrd /boot/initramfs-linux.img 
+	-monitor telnet:127.0.0.1:1235,server,nowait
 
 # -s makes qemu listen on 1234
+# -smp 1 sets it to single core
