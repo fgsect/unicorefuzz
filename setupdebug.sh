4fd2cc2cadd5d0768e6a2d956baa4cecb1ad4f2d
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2019-08-29 23:32:52 +0200

    python3: use explicit python3 executable where possible

--- setupdebug.sh
+++ setupdebug.sh
@@ -9,10 +9,10 @@ git checkout 7881cf8207a94f6fa88c3d07b9c629037a2a850e || exit 1
 if [[ "$VIRTUAL_ENV" != "" ]]
 then
   echo "[+] Installing dependencies in virtualenv"
-  pip install prompt-toolkit inquirer termcolor capstone keystone hexdump keystone_engine tabulate
+  pip3 install prompt-toolkit inquirer termcolor capstone hexdump keystone_engine tabulate
 else
   echo "[+] Installing dependencies as user"
-  pip install --user prompt-toolkit inquirer termcolor capstone keystone hexdump keystone_engine tabulate
+  pip3 install --user prompt-toolkit inquirer termcolor capstone hexdump keystone_engine tabulate
 fi
 
 echo "[+] Done. Harness.py -d will now work. Have a nice day."
