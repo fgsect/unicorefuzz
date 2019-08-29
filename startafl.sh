f44adb62dc256abe6a48cb04036e968f9c322164
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2019-08-29 23:23:23 +0200

    startafl.sh: also wait for probe_wrapper to start up

--- startafl.sh
+++ startafl.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export PATH=./AFLplusplus:$PATH
+
+python -c 'import utils; utils.wait_for_probe_wrapper()'
+
 if [ -z "$1" ]; then
 	echo "[*] Unicorefuzz running AFL master node. Supply an id to spawn additional workers."
 	afl-fuzz -U -m none -i afl_inputs -o afl_outputs -t 4000+ -M master -- python harness.py @@ || exit 1
